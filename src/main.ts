fe7a6e848f385c1d5ad5dcdffdfda862d5f89810
Elvan Hidayat <elvanhd@gmail.com>
2023-08-23 16:20:48 +0700

    setting up sessions

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,27 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const cookieSession = require('cookie-session');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['qwerty'],
+    }),
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   (app as any).set('etag', false);
   app.use((req, res, next) => {
     res.removeHeader('x-powered-by');
     res.removeHeader('date');
     next();
   });
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+
   await app.listen(3000);
 }
 
