a19a91e2c81216d5abb42a17c4d44a279c7ec9b0
Elvan Hidayat <elvanhd@gmail.com>
2023-08-24 22:21:24 +0700

    creating an auth integration test

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,11 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const cookieSession = require('cookie-session');
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['qwerty'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+
   (app as any).set('etag', false);
+
   app.use((req, res, next) => {
     res.removeHeader('x-powered-by');
     res.removeHeader('date');
